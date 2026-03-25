f7e177b5271c2b6b3edd178c46b657b1cddc1ec7
vgkamlesh <vgkamlesh2006@gmail.com>
2026-03-25 13:58:19 +0530

    Update App.jsx

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
       return;
     }
 
-    fetch(`http://localhost:3000/api/recommendation/${id}`, {
+    fetch(`https://electiverecommender.onrender.com/api/recommendation/${id}`, {
       signal: controller.signal
     })
       .then(res => {
