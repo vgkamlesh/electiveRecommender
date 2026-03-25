503ad26b37b4612ea010be7f446a25b8c011f3c6
vgkamlesh <vgkamlesh2006@gmail.com>
2026-03-25 14:11:57 +0530

    Update app.routes.ts

--- ang/src/app/app.routes.ts
+++ ang/src/app/app.routes.ts
@@ -4,5 +4,11 @@ import { Result } from './result/result';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'result', component: Result }
+  {
+    path: 'result',
+    redirectTo: () => {
+      window.location.href = 'https://elective-recommender-pk83.vercel.app/';
+      return '';
+    }
+  }
 ];
